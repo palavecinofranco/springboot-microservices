d5e619a4e9764d837a3172948914d190e0deedc4
palavecinofranco <francopalavecinoagus@gmail.com>
2023-07-14 12:39:33 -0300

    Add spring-security & resilience4j

---
+++ api-gateway/src/main/java/com/palavecinofranco/api_gateway/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.palavecinofranco.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception{
+        http
+                .csrf().disable()
+                .authorizeExchange().anyExchange().authenticated()
+                .and()
+                .oauth2Login();
+        return http.build();
+    }
+}
--- products-service/src/main/java/com/palavecinofranco/products_service/controllers/ProductController.java
+++ products-service/src/main/java/com/palavecinofranco/products_service/controllers/ProductController.java
@@ -6,6 +6,8 @@ import com.palavecinofranco.products_service.services.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,18 +15,21 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/product")
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class ProductController {
 
     private final ProductService productService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void addProduct(@RequestBody ProductRequest productRequest){
         this.productService.addProduct(productRequest);
     }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ROLE_USER')")
     public List<ProductResponse> getAllProducts(){
         return this.productService.getAllProducts();
     }
